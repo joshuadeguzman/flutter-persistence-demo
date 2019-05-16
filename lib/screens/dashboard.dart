3aa42f5fb6a24852ade3a52e5c6d49ec00e3e260
jdeguzman <jdeguzman@freelancer.com>
2019-05-16 19:32:34 +0800

    Add details on simple storage

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -31,23 +31,76 @@ class DashboardScreenState extends State<DashboardScreen> {
         title: Text('Flutter Persistence Demo'),
       ),
       body: Container(
-        width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: <Widget>[
-            Text('Persisted Counter: $_counter'),
-            RaisedButton(
-              child: Text('Increment'),
-              onPressed: _incrementCounter,
-            ),
-            RaisedButton(
-              child: Text('Decrement'),
-              onPressed: _decreaseCounter,
-            ),
-          ],
-        ),
-      ),
+          padding: EdgeInsets.all(20),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                'Storing simple data (shared_preferences 0.5.2)',
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 5)),
+              Text(
+                'Wraps NSUserDefaults (on iOS) and SharedPreferences (on Android). Not recommended for storing critical data.',
+                style: TextStyle(
+                  fontSize: 14,
+                ),
+              ),
+              Padding(padding: EdgeInsets.only(bottom: 10)),
+              ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: Container(
+                  color: Colors.blue,
+                  height: 175,
+                  padding: EdgeInsets.all(10),
+                  width: double.infinity,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        'Counter (int)',
+                        style: TextStyle(
+                          fontSize: 20,
+                          color: Colors.white,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 5)),
+                      Text(
+                        '$_counter',
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Padding(padding: EdgeInsets.only(bottom: 10)),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          RaisedButton(
+                            child: Text('Increment'),
+                            onPressed: _incrementCounter,
+                          ),
+                          Padding(padding: EdgeInsets.all(10)),
+                          RaisedButton(
+                            child: Text('Decrement'),
+                            onPressed: _decreaseCounter,
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )),
     );
   }
 
