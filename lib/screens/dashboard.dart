096f606f5131a71eff518f5bed7d9d807c9cff93
jdeguzman <jdeguzman@freelancer.com>
2019-05-16 18:55:32 +0800

    Initial commit

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -3,6 +3,7 @@
 // license that can be found in the LICENSE file.
 
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class DashboardScreen extends StatefulWidget {
   @override
@@ -12,14 +13,70 @@ class DashboardScreen extends StatefulWidget {
 }
 
 class DashboardScreenState extends State<DashboardScreen> {
+  final String KEY_COUNTER = 'counter';
+  int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Update view with the initial value
+    _retrievePersistedCounter();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text(
-          'Flutter Demo'
+      appBar: AppBar(
+        title: Text('Flutter Persistence Demo'),
+      ),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Text('Persisted Counter: $_counter'),
+            RaisedButton(
+              child: Text('Increment'),
+              onPressed: _incrementCounter,
+            ),
+            RaisedButton(
+              child: Text('Decrement'),
+              onPressed: _decreaseCounter,
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  _retrievePersistedCounter() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    int counter = (preferences.getInt(KEY_COUNTER) ?? 0) + 1;
+
+    setState(() {
+      _counter = counter;
+    });
+  }
+
+  _incrementCounter() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    int counter = (preferences.getInt(KEY_COUNTER) ?? 0) + 1;
+    await preferences.setInt(KEY_COUNTER, counter);
+
+    setState(() {
+      _counter = counter;
+    });
+  }
+
+  _decreaseCounter() async {
+    SharedPreferences preferences = await SharedPreferences.getInstance();
+    int counter = (preferences.getInt(KEY_COUNTER) ?? 0) - 1;
+    await preferences.setInt(KEY_COUNTER, counter);
+
+    setState(() {
+      _counter = counter;
+    });
   }
 }
